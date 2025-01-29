8414564b3fc703b3db40bb35d9a0aeb20f8b95a5
Tagliaferro22 <mateog1238@gmail.com>
2025-01-29 17:31:46 -0300

    Desarrollando mi primer DAG

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 27/1/2025: ¡Hola! En este repositorio voy a ir subiendo lo que aprenda de Airlow, incorporaré el aprendizaje mediante mini-proyectos, que eventualmente me permitirían construir un proyecto más complejo.
 
 28/1: Hoy estuve revisando formas de correr docker, y hablando con un colega me recomendó usar Ubuntu por el WSL2, el cuál, investigando un poco ví que es 20 veces más rápido que el WSL. Así que a partir de mañana voy a comenzar a correr Docker usando WSL2, para posteriormente poder usar Airflow en el contenedor de Docker. 
+
+29/1: Hoy por fín pude iniciar el web-server, me sentí un hacker ajajjaja, a continuación voy a comenzar mis primeras pruebas en Airflow.
--- dags/my_first_dag.py
+++ dags/my_first_dag.py
@@ -1,13 +1,47 @@
 from airflow import DAG
-from airflow.operators.python import PythonOperator, BranchPythonOperator
-from datetime import datetime
+from airflow.operators.bash import BashOperator  
+from airflow.operators.python import PythonOperator 
+from datetime import datetime, timedelta
 from random import randint
 
-def _training_model():
-    return randint(1,10)
+TAGS =  ["Mi primer dag"] # Un tag para posteriormente poder filtrar el DAG
+DAG_ID = "MI_PRIMER_DAG"
+DAG_DESCRIPTION = """El primer DAG que programo en mi vida, hola mundo"""
+DAG_SCHEDULE = "0 9 * * *" # Se ejecuta todos los días a las 9AM
 
-with DAG("my_first_dag_ever", start_date=datetime(2021,1,1), schedule_interval="@daily",catchup=False) as dag:
-    training_model_A = PythonOperator(
-        task_id = "training_model_A"
-        python_callable = _training_model
+retries = 4 # La cantidad de reintentos
+retry_delay = timedelta(minutes=5) # Va a tomarse una pausa de 5 minutos entre cada reintento
+
+def execute_task():
+    return f"¡Hola, mundo! {randint(1,10)}"
+
+dag = DAG(
+    dag_id = DAG_ID,
+    description = DAG_DESCRIPTION,
+    catchup = False,
+    schedule_interval = DAG_SCHEDULE,
+    max_active_runs = 1,
+    dagrun_timeout = timedelta(minutes=3, seconds=20),
+    default_args = {"start_date" : datetime(2025, 1, 29)},
+    tags=TAGS
+)
+
+with dag as using_dag:
+    start_task = BashOperator(
+        task_id = "inicia_el_proceso",
+        bash_command='echo "Esta tarea no hace nada"'
     )
+
+    end_task = BashOperator(
+        task_id = "finaliza_el_proceso",
+        bash_command='echo "Esta tarea no hace nada"'
+    )
+
+    first_task = PythonOperator(
+        task_id = "primera_tarea",
+        python_callable = execute_task,
+        retries = retries,
+        retry_delay = retry_delay
+    )
+
+start_task >> first_task >> end_task # Establecemos las dependencias de las tareas, first_task depende de que se ejecute start_task, mientras que end_task depende de que se ejecuten tanto start_task cómo luego first_task
